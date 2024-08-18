baa95dd2a3eeed1fe11dacdf6687bf68db6becb4
pseudowasabi <pseudowasabi@kakao.com>
2024-08-18 19:31:27 +0900

    leetcode 1768

---
+++ leetcode/merge-strings-alternately.kt
@@ -0,0 +1,14 @@
+class Solution {
+    fun mergeAlternately(word1: String, word2: String): String {
+        var i = 0; var j = 0
+        val mergedString: StringBuilder = StringBuilder()
+        while (i < word1.length || j < word2.length) {
+            if (i < word1.length)
+                mergedString.append(word1[i])
+            if (j < word2.length)
+                mergedString.append(word2[j])
+            i++; j++
+        }
+        return mergedString.toString()
+    }
+}
