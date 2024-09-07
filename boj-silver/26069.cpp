26ce471462279c92134515ca55a5dce20400213f
pseudowasabi <pseudowasabi@kakao.com>
2024-09-07 11:32:06 +0900

    boj 26069(wa/2nd)

--- boj-silver/26069.cpp
+++ boj-silver/26069.cpp
@@ -20,50 +20,41 @@ int main() {
     string a, b;
     int aIndex, bIndex;
 
-    bool startFlag = false;
+    int firstAppeared = -1;
 
     for (int i = 0; i < n; ++i) {
         cin >> a >> b;
 
-        if (!startFlag && (!a.compare("ChongChong") || !b.compare("ChongChong")) && a.compare(b)) {
-            startFlag = true;
+        if (a == b) {
+            continue;
         }
 
-        if (startFlag) {
-            if (a.compare("ChongChong")) {
-                if (nameToIndex.find(a) != nameToIndex.end()) {
-                    aIndex = nameToIndex.find(a)->second;
-                } else {
-                    nameToIndex.insert(make_pair(a, ++indexCount));
-                    aIndex = indexCount;
-                }
+        if (a.compare("ChongChong")) {
+            if (nameToIndex.find(a) != nameToIndex.end()) {
+                aIndex = nameToIndex.find(a)->second;
             } else {
-                aIndex = 0;
+                nameToIndex.insert(make_pair(a, ++indexCount));
+                aIndex = indexCount;
             }
-            if (b.compare("ChongChong")) {
-                if (nameToIndex.find(b) != nameToIndex.end()) {
-                    bIndex = nameToIndex.find(b)->second;
-                } else {
-                    nameToIndex.insert(make_pair(b, ++indexCount));
-                    bIndex = indexCount;
-                }
-            } else {
-                bIndex = 0;
-            }
-            
-            matrix[aIndex][bIndex] = matrix[bIndex][aIndex] = 1;
+        } else {
+            firstAppeared = firstAppeared == -1 ? i + 1 : firstAppeared;
+            aIndex = 0;
         }
+
+        if (b.compare("ChongChong")) {
+            if (nameToIndex.find(b) != nameToIndex.end()) {
+                bIndex = nameToIndex.find(b)->second;
+            } else {
+                nameToIndex.insert(make_pair(b, ++indexCount));
+                bIndex = indexCount;
+            }
+        } else {
+            firstAppeared = firstAppeared == -1 ? i + 1 : firstAppeared;
+            bIndex = 0;
+        }
+        
+        matrix[aIndex][bIndex] = matrix[bIndex][aIndex] = i + 1;
     }
-
-    queue<int> nextSearchTarget;
-    nextSearchTarget.push(0);
-
-    int numberOfPeople = indexCount + 1;
-    int alreadyChecked[numberOfPeople];
-    memset(alreadyChecked, 0, sizeof(alreadyChecked));
-
-    alreadyChecked[0] = 1;
-    int result = 1;
 
     // for (int i = 0; i < n; ++i) {
     //     for (int j = 0; j < n; ++j) {
@@ -72,11 +63,42 @@ int main() {
     //     cout << '\n';
     // }
 
+    // for (auto i : nameToIndex) {
+    //     cout << i.first << ' ' << i.second << '\n';
+    // }
+
+    // cout << "first appeared: " << firstAppeared << '\n';
+
+    int numberOfPeople = nameToIndex.size() + 1;
+    for (int i = 0; i < numberOfPeople; ++i) {
+        for (int j = 0; j < numberOfPeople; ++j) {
+            matrix[i][j] = matrix[i][j] < firstAppeared ? 0 : matrix[i][j];
+        }
+    }
+
+    // for (int i = 0; i < n; ++i) {
+    //     for (int j = 0; j < n; ++j) {
+    //         cout << matrix[i][j] << ' ';
+    //     }
+    //     cout << '\n';
+    // }
+
+    queue<int> nextSearchTarget;
+    nextSearchTarget.push(0);
+
+
     // cout << numberOfPeople << '\n';
+
+
+    int alreadyChecked[numberOfPeople];
+    memset(alreadyChecked, 0, sizeof(alreadyChecked));
+
+    alreadyChecked[0] = 1;
+    int result = 1;
 
     while (!nextSearchTarget.empty()) {
         for (int i = 0; i < numberOfPeople; ++i) {
-            if (matrix[nextSearchTarget.front()][i] == 1 && alreadyChecked[i] != 1) {
+            if (matrix[nextSearchTarget.front()][i] > 0 && alreadyChecked[i] != 1) {
                 alreadyChecked[i] = 1;
                 nextSearchTarget.push(i);
                 // cout << nextSearchTarget.front() << ' ' << i << '\n';
