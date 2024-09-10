373659d2750fe9dbb2e469cfe22cd98555d88242
pseudowasabi <pseudowasabi@kakao.com>
2024-09-11 00:05:14 +0900

    boj 25501(wa/2nd)

--- boj-bronze/25501.cpp
+++ boj-bronze/25501.cpp
@@ -1,7 +1,17 @@
 #include <iostream>
 #include <string>
-#include <algorithm>
+#include <utility>
 using namespace std;
+
+pair<int, int> recursion(string s, int l, int r, int depth) {
+    if(l >= r) return make_pair(1, depth);
+    else if(s[l] != s[r]) return make_pair(0, depth);
+    else return recursion(s, l+1, r-1, depth + 1);
+}
+
+pair<int, int> isPalindrome(string s) {
+    return recursion(s, 0, s.length() - 1, 1);
+}
 
 int main() {
     ios_base::sync_with_stdio(false);
@@ -12,16 +22,8 @@ int main() {
     for (string s; t > 0; --t) {
         cin >> s;
 
-        string s2 = s;
-        reverse(s2.begin(), s2.end());
-
-        if (!s.compare(s2)) {
-            cout << "1 ";
-        } else {
-            cout << "0 ";
-        }
-
-        cout << '\n';
+        pair<int, int> result = isPalindrome(s);
+        cout << result.first << ' ' << result.second << '\n';
     }
     
     return 0;
